9ef65e5dcd9f5c75e4575e98fc8a29caad6d43af
julien Berthou - grifdail <julien@grifdail.fr>
2018-01-27 13:54:07 +0100

    first prototype

--- Raspberry/index.js
+++ Raspberry/index.js
@@ -1,7 +1,10 @@
 var app = require('http').createServer(handler)
 var io = require('socket.io')(app);
 var fs = require('fs');
+var Gpio = require('onoff').Gpio,
 
+
+button = new Gpio(4, 'in', 'both');
 app.listen(80);
 
 function handler (req, res) {
@@ -10,8 +13,14 @@ function handler (req, res) {
 }
 
 io.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+  console.log("Connection found");
+  button.watch(function(err, value) {
+    console.log("button value "+value);
+    socket.emit("button pressed");
+  });
+  console.log("Connection found");
+  socket.emit('connectionConfirmed');
+  socket.on('phone message', function (data) {
+    console.log("new phone message"data);
   });
 });
