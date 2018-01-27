a3a1a4710fb23c54103f8c51fad2b8830c222b09
LY0119\Administrateur <simhembert@gmail.com>
2018-01-27 17:05:10 +0100

    1st commit unity project

---
+++ Phone/Assets/Scripts/RaspberryInfos.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class ReaspberryInfos
+{
+	public string instruction;
+	public string challengeType;
+	public string[] sequenceList;
+	public int timeLeft;
+	public int challengeLeft;
+
+	public static ReaspberryInfos CreateFromJSON(string jsonString)
+	{
+		return JsonUtility.FromJson<ReaspberryInfos>(jsonString);
+	}
+}
