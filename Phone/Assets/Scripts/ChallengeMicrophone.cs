7e38ee81b0c7f6a827e19f46f0f16bff2cd048b0
julien Berthou - grifdail <julien@grifdail.fr>
2018-01-28 11:31:33 +0100

    add sound explosion

---
+++ Phone/Assets/Scripts/ChallengeMicrophone.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChallengeMicrophone : MonoBehaviour {
+
+    
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        AudioSource aud = GetComponent<AudioSource>();
+        aud.clip = Microphone.Start("Built-in Microphone", true, 10, 44100);
+        aud.Play();
+    }
+}
